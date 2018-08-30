001dd20b9ee044ffae86edc89ab2c63f56724148
StevenNagata <nagata.steven1@gmail.com>
2018-08-29 17:58:57 -0700

    Call update function on a one second interval when the button is clicked.

--- main.js
+++ main.js
@@ -3,9 +3,15 @@ var $startbutton = document.getElementById('startbutton')
 var currentTime = parseInt($currentTime.textContent, 10)
 
 function timeStart() {
+
   var timeIncrease = setInterval(function () {
     $currentTime.textContent = currentTime += 1
   }, 1000)
 }
 
 $startbutton.addEventListener('click', timeStart, false)
+
+
+function timePause() {
+
+}
