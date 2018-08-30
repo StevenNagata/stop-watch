4ad9e0150339c2fe9ea39088432b006e428a22ce
StevenNagata <nagata.steven1@gmail.com>
2018-08-29 22:03:35 -0700

    Swap the play icon for a pause icon to indicate the state of the watch.

--- main.js
+++ main.js
@@ -1,12 +1,35 @@
-var $currentTime = document.getElementById('time')
-var $startbutton = document.getElementById('startbutton')
-var currentTime = parseInt($currentTime.textContent, 10)
+var $time = document.getElementById('time')
+var $startPause = document.getElementById('startPause')
+var currentTime = parseInt($time.textContent, 10)
 
+
+$startPause.addEventListener('click', function () {
+
+  var runState = $startPause.textContent
+
+  if (runState === "Start") {
+    var intervalId = setInterval(function () {
+      currentTime += 1
+      $time.textContent = currentTime
+    }, 1000)
+    $startPause.textContent = "Pause"
+  }
+
+  if (runState === "Pause") {
+    clearInterval(1)
+    $startPause.textContent = "Start"
+  }
+
+})
+
+
+/*
 function timeStart() {
 
   var timeIncrease = setInterval(function () {
     $currentTime.textContent = currentTime += 1
   }, 1000)
+
 }
 
 $startbutton.addEventListener('click', timeStart, false)
@@ -15,3 +38,4 @@ $startbutton.addEventListener('click', timeStart, false)
 function timePause() {
 
 }
+*/
