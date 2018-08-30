ac02113960a0dce143800c14ec35d1a84d1d1f8e
StevenNagata <nagata.steven1@gmail.com>
2018-08-30 15:40:44 -0700

    Updated my IncreaseTime function to stop at the given Time limit set by user #4

--- main.js
+++ main.js
@@ -2,14 +2,25 @@ var $time = document.getElementById('time')
 var $startPause = document.getElementById('startPause')
 var currentTime = parseInt($time.textContent, 10)
 var $reset = document.getElementById('reset')
+var $timeLimit = document.getElementById('timeLimit')
 var intervalId;
+
 
 $startPause.addEventListener('click', function () {
   var runState = $startPause.textContent
+  var maxTime = parseInt($timeLimit.value, 10)
+
   function increaseTime() {
-    currentTime += 1
-    $time.textContent = currentTime
+    if (maxTime <= currentTime) {
+      clearInterval(intervalId)
+
+    }
+    else {
+      currentTime += 1
+      $time.textContent = currentTime
+    }
   }
+
   if (runState === "Start") {
     clearInterval(intervalId)
     intervalId = setInterval(increaseTime, 1000)
@@ -27,11 +38,11 @@ $startPause.addEventListener('click', function () {
 })
 
 $reset.addEventListener('click', function () {
-    clearInterval(intervalId)
-    currentTime = 0
-    $time.textContent = 0
-    $startPause.textContent = "Start"
-    $startPause.classList.add('green')
-    $startPause.classList.remove('red')
-    $reset.classList.remove('display')
+  clearInterval(intervalId)
+  currentTime = 0
+  $time.textContent = 0
+  $startPause.textContent = "Start"
+  $startPause.classList.add('green')
+  $startPause.classList.remove('red')
+  $reset.classList.remove('display')
 })
